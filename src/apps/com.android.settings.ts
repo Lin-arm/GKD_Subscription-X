e567b5a4f083b602df6d770241f705e8f41c673b
Lin-arm <85251920+Lin-arm@users.noreply.github.com>
2026-02-12 15:41:43 +0800

    v47.0.4 设置 功能类-允许USB调试-确定

--- src/apps/com.android.settings.ts
+++ src/apps/com.android.settings.ts
@@ -28,17 +28,35 @@ export default defineGkdApp({
     {
       key: 2,
       name: '自动精灵-获取无障碍-允许',
-      desc: '(真我📱)弹窗-点击 允许',
+      desc: '(真我📱)弹窗-点击[允许]',
       enable: false,
       rules: [
         {
+          fastQuery: true,
           matches: [
             '[vid="title"][text*="自动精灵"]',
             '[text="允许"][clickable=true]',
           ],
-          fastQuery: true,
           snapshotUrls: 'https://i.gkd.li/i/23588703',
           activityIds: '.SubSettings',
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '功能类-允许USB调试-确定',
+      desc: '(真我📱)弹窗-点击[确定]',
+      enable: false,
+      rules: [
+        {
+          fastQuery: true,
+          matches: [
+            '[text="是否允许 USB 调试？"][visibleToUser=true]',
+            '[text="确定"][clickable=true]',
+          ],
+          snapshotUrls: 'https://i.gkd.li/i/25254118',
+          exampleUrls: 'https://e.gkd.li/31684fe6-d89d-4e56-a4a3-2543a462ace8',
+          activityIds: '.Settings$DevelopmentSettingsDashboardActivity',
         },
       ],
     },
