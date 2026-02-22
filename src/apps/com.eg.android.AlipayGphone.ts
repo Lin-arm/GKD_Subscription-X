5054d4ff082cb548ea12b8180399e924177ca9cf
Lin-arm <85251920+Lin-arm@users.noreply.github.com>
2026-02-23 00:04:23 +0800

    fix: 支付宝 森林-集市-弹窗-x掉

--- src/apps/com.eg.android.AlipayGphone.ts
+++ src/apps/com.eg.android.AlipayGphone.ts
@@ -512,13 +512,14 @@ export default defineGkdApp({
           key: 3,
           name: '③专享补贴or添加首页-放弃',
           matches: [
-            '[text$="可用" || text="后失效" || text="限时领取" || text="限今日"] +(1,2) TextView[text$="放弃"][index=parent.childCount.minus(2)]',
+            '[text$="可用" || text="后失效" || text^="限时" || text="限今日"] +(1,2) TextView[text$="放弃"][index=parent.childCount.minus(2)]',
           ],
           snapshotUrls: [
             'https://i.gkd.li/i/24157391', //专享补贴
             'https://i.gkd.li/i/24278961', //添加小程序到首页 (若用快速查询则真机不生效)
             'https://i.gkd.li/i/24913115', //添加小程序到首页2
             'https://i.gkd.li/i/25063019', //天猫小程序入群福利
+            'https://i.gkd.li/i/25470291', //天猫小程序
           ],
         },
         {
