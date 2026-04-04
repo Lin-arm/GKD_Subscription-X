9bf041fa20e22a58cde25e1aa9c096b6d35f7c31
Lin-arm <85251920+Lin-arm@users.noreply.github.com>
2026-04-04 21:51:37 +0800

    feat: 小红书 功能类-分享时自动[复制链接]

---
+++ src/apps/com.xingin.xhs.ts
@@ -0,0 +1,24 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'com.xingin.xhs',
+  name: '小红书',
+  groups: [
+    {
+      key: 1,
+      name: '功能类-分享时自动[复制链接]',
+      desc: '复制到wx抖快小程序解析,可无水印下载',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.xingin.matrix.notedetail.NoteDetailActivity',
+          matches: '@[clickable=true] >(1,2) ImageView + [text="复制链接"]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/26480990', //点击帖子右上角分享后出现
+            'https://i.gkd.li/i/26480989', //长按图片后出现
+          ],
+        },
+      ],
+    },
+  ],
+});
