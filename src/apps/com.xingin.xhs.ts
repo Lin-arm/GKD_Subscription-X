b9d8565e748b8af876434a310ddd23b568bd0235
Lin-arm <85251920+Lin-arm@users.noreply.github.com>
2026-04-04 22:17:26 +0800

    perf: 小红书

--- src/apps/com.xingin.xhs.ts
+++ src/apps/com.xingin.xhs.ts
@@ -8,15 +8,21 @@ export default defineGkdApp({
       key: 1,
       name: '功能类-分享时自动[复制链接]',
       desc: '复制到wx抖快小程序解析,可无水印下载',
+      fastQuery: true,
+      activityIds: 'com.xingin.matrix.notedetail.NoteDetailActivity',
       rules: [
         {
-          fastQuery: true,
-          activityIds: 'com.xingin.matrix.notedetail.NoteDetailActivity',
-          matches: '@[clickable=true] >(1,2) ImageView + [text="复制链接"]',
-          snapshotUrls: [
-            'https://i.gkd.li/i/26480990', //点击帖子右上角分享后出现
-            'https://i.gkd.li/i/26480989', //长按图片后出现
-          ],
+          key: 0,
+          name: '①右上角分享-[复制链接]',
+          matches: '@[vid="shareLayout"][clickable=true] >2 [text="复制链接"]',
+          snapshotUrls: 'https://i.gkd.li/i/26480990', //点击帖子右上角分享后出现
+        },
+        {
+          key: 1,
+          name: '②长按图片-[复制链接]',
+          actionCd: 300,
+          matches: '@[clickable=true] > ImageView + [text="复制链接"]',
+          snapshotUrls: 'https://i.gkd.li/i/26480989', //长按图片后出现
         },
       ],
     },
