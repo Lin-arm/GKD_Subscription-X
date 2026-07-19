b4c9e1839701fa192fdae0e1ec1f0eaea306d231
Lin-arm <85251920+Lin-arm@users.noreply.github.com>
2026-07-19 22:23:49 +0800

    feat: 手机管家 应用信息页-强行停止

--- src/apps/com.miui.securitycenter.ts
+++ src/apps/com.miui.securitycenter.ts
@@ -6,17 +6,30 @@ export default defineGkdApp({
   groups: [
     {
       key: 1,
-      name: '应用信息页-开自启动-确定',
-      desc: '弹窗-确定',
+      name: '应用信息页-开自启动',
+      desc: '弹窗-点击[确定]',
       enable: false,
       rules: [
         {
-          matches: [
-            '[vid="message"][text^="开启自启动后"]',
-            '[text="确定"][clickable=true]',
-          ],
           fastQuery: true,
+          matches: ['[text*="自启动"]', '[text="确定"][clickable=true]'],
           snapshotUrls: 'https://i.gkd.li/i/23764176',
+          exampleUrls: 'https://e.gkd.li/9a021f5f-8ade-47a2-8923-6038b85c8587',
+          activityIds: 'com.miui.appmanager.ApplicationsDetailsActivity',
+        },
+      ],
+    },
+    {
+      key: 2,
+      name: '应用信息页-强行停止',
+      desc: '弹窗-点击[确定]',
+      enable: false,
+      rules: [
+        {
+          fastQuery: true,
+          matches: ['[text*="强行停止"]', '[text="确定"][clickable=true]'],
+          snapshotUrls: 'https://i.gkd.li/i/30166108',
+          exampleUrls: 'https://e.gkd.li/f0a6acc3-e51c-41dd-be9d-067973b8a912',
           activityIds: 'com.miui.appmanager.ApplicationsDetailsActivity',
         },
       ],
