9dc48d7f4b47f6c93d01f50c9705df9aafe328ec
Lin-arm <85251920+Lin-arm@users.noreply.github.com>
2026-08-02 23:31:29 +0800

    perf: 一淘
    - 移除更新提示
    - 其它...

--- src/apps/com.taobao.etao.ts
+++ src/apps/com.taobao.etao.ts
@@ -5,31 +5,25 @@ export default defineGkdApp({
   name: '一淘',
   groups: [
     {
-      key: 1,
-      name: '首页-更新弹窗-取消',
-      desc: '点击取消',
-      rules: [
-        {
-          // versionCode: { include: 975 },
-          matches: ['[text="更新提示"]', 'Button[text="取消"][clickable=true]'],
-          fastQuery: true,
-          snapshotUrls: 'https://i.gkd.li/i/23607482',
-          activityIds: '.app.homev4.HomeV4Activity',
-        },
-      ],
-    },
-    {
       key: 2,
-      name: '签到-自动签到',
-      desc: '点击签到',
+      name: '功能类-自动[签到]',
+      desc: '签到领钱页-点击[签到]',
       rules: [
         {
+          fastQuery: true,
+          matchRoot: true,
           matchDelay: 1000,
+          actionDelay: 300,
           actionMaximum: 1,
           resetMatch: 'app',
-          matches: '[text="签到"] - View[childCount=3] > Image[index=0]',
-          snapshotUrls: 'https://i.gkd.li/i/23643957',
-          activityIds: ['com.taobao.sns.activity.ISWebViewActivity'],
+          matches:
+            '[text="签到"] - @View[childCount=3][getChild(0).name$="Image"] <<(30-n) [desc="WVUCWebView"] <<6 [id="android:id/content"]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/30633488',
+            'https://i.gkd.li/i/30633316',
+          ],
+          exampleUrls: 'https://e.gkd.li/c996b5fb-58f5-46f1-9070-49af596e6afb',
+          activityIds: 'com.taobao.sns.activity.ISWebViewActivity',
         },
       ],
     },
@@ -44,8 +38,9 @@ export default defineGkdApp({
             '@[text="打开"] <2 [childCount=2] < ScrollView - FrameLayout >3 [text^="正在离开一淘"]',
           snapshotUrls: [
             'https://i.gkd.li/i/23239468',
-            'https://i.gkd.li/i/29088356',
+            'https://i.gkd.li/i/30633516',
           ],
+          exampleUrls: 'https://e.gkd.li/1ccc4813-4a29-4d47-98e8-5f1a1699fc93',
           activityIds: [
             'com.taobao.sns.activity.ISWebViewActivity',
             'com.taobao.sns.tms.CommonTMSActivity',
@@ -55,15 +50,16 @@ export default defineGkdApp({
     },
     {
       key: 4,
-      name: '签到-🧧领取现金奖励',
-      desc: '已做任务->点击[领取]',
+      name: '功能类-自动[领取]奖励',
+      desc: '签到领钱页-已做任务->点击[领取]',
       actionCd: 1500,
       activityIds: 'com.taobao.sns.activity.ISWebViewActivity',
       rules: [
         {
           key: 1,
           matches: '@[text="领取"] - View >2 [text="现金"][visibleToUser=true]',
-          snapshotUrls: 'https://i.gkd.li/i/22974322',
+          snapshotUrls: 'https://i.gkd.li/i/30634063',
+          exampleUrls: 'https://e.gkd.li/afb9bd2b-9b3d-45b5-933a-e10b0e6cc639',
         },
         {
           key: 2,
@@ -88,8 +84,12 @@ export default defineGkdApp({
       rules: [
         {
           key: 1,
-          matches: '@[desc="返回"] + LinearLayout > [text="正在跳转"]',
-          snapshotUrls: 'https://i.gkd.li/i/29088626',
+          matches: '@[desc="返回"] + LinearLayout > [vid="tvTitle"]',
+          snapshotUrls: [
+            'https://i.gkd.li/i/29088626',
+            'https://i.gkd.li/i/30633611',
+          ],
+          exampleUrls: 'https://e.gkd.li/dcd95b76-aed7-453d-88cd-b794e949dbf2',
         },
         {
           key: 2,
